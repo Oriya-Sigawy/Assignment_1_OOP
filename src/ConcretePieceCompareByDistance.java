21fdb7935b289cc7bad10b2f87a9285fbc1e568b
oriya <oriyas.07@gmail.com>
2024-01-14 18:33:29 +0200

    changed ConcretePieceCompareByDistance to comply with test

--- src/ConcretePieceCompareByDistance.java
+++ src/ConcretePieceCompareByDistance.java
@@ -5,7 +5,7 @@ public class ConcretePieceCompareByDistance implements Comparator<ConcretePiece>
     public int compare(ConcretePiece o1, ConcretePiece o2) {
         if (o1.getDist() == o2.getDist()) {
             if (o1.getNum() == o2.getNum()) {
-                return Boolean.compare(o1.getOwner().equals(GameLogic.winner), o2.getOwner().equals(GameLogic.winner));
+                return Boolean.compare(o2.getOwner().equals(GameLogic.winner), o1.getOwner().equals(GameLogic.winner));
             }
             return Integer.compare(o1.getNum(), o2.getNum());
         }
